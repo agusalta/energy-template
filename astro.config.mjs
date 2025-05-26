8d7f7b1d00cd518d905a9ec9050c766235feea7d
agus <agusalta002@gmail.com>
2025-05-26 20:17:24 -0300

    Actualización de la configuración de Astro para incluir nuevos parámetros de construcción y ajuste de rutas de imágenes de clientes en Clients.astro. Se cambió la versión de NODE en netlify.toml.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   site: "https://vbartalis.github.io",
-  base: "/energy-template"
+  base: "/energy-template",
+  build: {
+    assets: "_assets",
+  },
+  vite: {
+    build: {
+      assetsInlineLimit: 0,
+    },
+  },
 });
