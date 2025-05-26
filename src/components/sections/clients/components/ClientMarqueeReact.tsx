4bd3148ea0a45112f348dbcf74b662071817f63e
agus <agusalta002@gmail.com>
2025-05-26 20:10:01 -0300

    Refactorización de importaciones en ClientMarqueeReact.tsx para simplificar el código y mejorar la legibilidad.

--- src/components/sections/clients/components/ClientMarqueeReact.tsx
+++ src/components/sections/clients/components/ClientMarqueeReact.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import Marquee from 'react-fast-marquee';
 import { useThemeStore } from '../../../../store/themeStore';
 
